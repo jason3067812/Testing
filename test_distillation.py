4685899ab1b859f6ab12b23d46ab88b83f8d2b82
Eric Chang <jason3067812@gmail.com>
2025-03-19 11:09:44 -0700

    8u7y6t5r4e3w2

--- test_distillation.py
+++ test_distillation.py
@@ -1,36 +1,4 @@
-# Copyright (c) 2024 Intel Corporation
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 
-import copy
-
-import unittest
-from datasets import load_dataset, load_metric
-from intel_extension_for_transformers.transformers import (
-    metrics,
-    OptimizedModel,
-)
-
-from neural_compressor.config import (
-    DistillationConfig,
-    KnowledgeDistillationLossConfig,
-)
-from intel_extension_for_transformers.transformers.trainer import NLPTrainer
-from transformers import (
-    AutoModelForSequenceClassification,
-    AutoTokenizer,
-    set_seed,
-)
 
 os.environ["WANDB_DISABLED"] = "true"
 
